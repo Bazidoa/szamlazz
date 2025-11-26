2ae16fc0d82e44ce7e540ddf993a2c95ce003461
Balazs Gyongyosi <balazs.gyoengyoesi.ext@dwpbank.de>
2025-11-26 11:34:44 +0100

    added update create and delete with validation and mapper

--- src/main/java/com/example/szamlazz/service/UserService.java
+++ src/main/java/com/example/szamlazz/service/UserService.java
@@ -1,14 +1,18 @@
 package com.example.szamlazz.service;
 
 import com.example.szamlazz.model.User;
+import com.example.szamlazz.model.UserVo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 public interface UserService {
     public Page<User> findUsers(Pageable pageable);
+
     public List<User> getUsers();
-    public Long createUser();
+
+    public Long saveUser(UserVo request);
+
+    public void deleteUser(Long userId);
 }
