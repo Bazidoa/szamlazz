7b80c146a93be3c8eb46a417effa48089edcba59
Balazs Gyongyosi <balazs.gyoengyoesi.ext@dwpbank.de>
2025-11-27 12:08:03 +0100

    add some tests to backend

--- target/generated-sources/annotations/com/example/szamlazz/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/com/example/szamlazz/mapper/UserMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-11-26T17:13:03+0100",
+    date = "2025-11-27T12:00:12+0100",
     comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.6 (Amazon.com Inc.)"
 )
 @Component
